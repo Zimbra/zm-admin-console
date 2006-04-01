5ca53328a987b1a1dc38755d64075d3b7a40436f
Unknown Perforce User <ccao>
2006-03-31 20:31:28 -0600

    http://bugzilla.zimbra.com/show_bug.cgi?id=6898 bug: 6898 invalidate the tooltip after modify the account Also fix the following bug: modify a resource account and click close -> when prompted by save or not, click yes. A javascript error is thrown 	The issue is that the resource javascript object's attrs are all arrays 	Modify the resource.initFromJS and enforce the attrs to be new object always
    Copied from Perforce
     Change: 23677

--- WebRoot/js/zimbraAdmin/resource/model/ZaResource.js
+++ WebRoot/js/zimbraAdmin/resource/model/ZaResource.js
@@ -251,6 +251,8 @@ function(mods) {
 	params.soapDoc = soapDoc;	
 	resp = modifyAccCommand.invoke(params).Body.ModifyCalendarResourceResponse;
 	this.initFromJS(resp.calresource[0]);
+	//invalidate the original tooltip
+	this._toolTip = null ;
 	return;
 }
 ZaItem.modifyMethods["ZaResource"].push(ZaResource.modifyMethod);
@@ -300,6 +302,9 @@ ZaResource.prototype.initFromJS =
 function (resource) {
 	if(!resource)
 		return;
+	
+	//ensure current attrs are empty. Otherwise, the old attributes will be included also	
+	this.attrs = new Object();			
 	this.name = resource.name;
 	this.id = resource.id;
 	var len = resource.a.length;
@@ -313,10 +318,8 @@ function (resource) {
 			this.attrs[[resource.a[ix].n]].push(resource.a[ix]._content);
 		}
 	}
-	
-	//TODO: define the A_schedulePolicy according
-	this.setSchedulePolicyFromLdapAttrs();
-	
+		
+	this.setSchedulePolicyFromLdapAttrs();	
 }
 
 //set the ldap attributes according to the schedule policy values
