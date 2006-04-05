8b19e9d404fc6bb8ff5fd7fdc05bffcca600e5ab
Greg Solovyev <greg@zimbra.com>
2006-04-05 14:35:03 -0500

    http://bugzilla.zimbra.com/show_bug.cgi?id=4658 bug 4658  - when reindexing returns "SERVICE_NOT_RUNNING" on the first status request after it was started - consider it complete  - add "reindexing process started" and "reindexing complete" progress messages
    Copied from Perforce
     Change: 23836

--- WebRoot/js/zimbraAdmin/accounts/model/ZaAccount.js
+++ WebRoot/js/zimbraAdmin/accounts/model/ZaAccount.js
@@ -780,12 +780,15 @@ function (respObj, instance) {
 	if(respObj.isException && respObj.isException()) {
 		var errCode = respObj.getException().code;
 		if(errCode && errCode == "service.NOT_IN_PROGRESS") {
-			instance.status = null;
 			instance.errorDetail = "";
 			instance.resultMsg = "";	
 			instance.progressMsg = ZaMsg.NAD_ACC_ReindexingNotRunning;
-			if(instance.numRemaining > 0) {
+			if(instance.numRemaining > 0 || instance.status == "started") {
 				instance.numDone = instance.numTotal;
+				instance.status = "complete";	
+				instance.progressMsg = ZaMsg.NAD_ACC_ReindexingComplete;			
+			} else {
+				instance.status = null;
 			}
 		} else {
 			instance.resultMsg = String(ZaMsg.FAILED_REINDEX).replace("{0}", errCode);
@@ -802,6 +805,10 @@ function (respObj, instance) {
 		}
 		if(resp && resp.Body.ReIndexResponse) {
 			instance.status = resp.Body.ReIndexResponse.status;
+			if(instance.status == "started") {
+				instance.numDone = 0;
+				instance.progressMsg = ZaMsg.NAD_ACC_ReindexingStarted;							
+			}
 			if(resp.Body.ReIndexResponse.progress && resp.Body.ReIndexResponse.progress[0]) {
 				var progress = resp.Body.ReIndexResponse.progress[0];
 				instance.numFailed = progress.numFailed;
