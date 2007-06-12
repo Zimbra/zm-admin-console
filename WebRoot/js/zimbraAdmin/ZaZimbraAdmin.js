fe7295bd57136d855af2027ddf8c6379b4810896
Greg Solovyev <greg@zimbra.com>
2007-06-12 14:08:53 -0500

    bug 17512 do not initialize UI if we got auth exception (integrate 55316 from main)
    http://bugzilla.zimbra.com/show_bug.cgi?id=17512
    
    Copied from Perforce
     Change: 55688

--- WebRoot/js/zimbraAdmin/ZaZimbraAdmin.js
+++ WebRoot/js/zimbraAdmin/ZaZimbraAdmin.js
@@ -307,10 +307,12 @@ function() {
 		
 	} catch (ex) {
 
-		if(!ZaSettings.initialized)
-			ZaSettings.init();
-		else
-			ZaZimbraAdmin._killSplash();
+		if(ex && ex.code != ZmCsfeException.NO_AUTH_TOKEN && ex.code != ZmCsfeException.SVC_AUTH_EXPIRED) {
+			if(!ZaSettings.initialized)
+				ZaSettings.init();
+			else
+				ZaZimbraAdmin._killSplash();
+		}	
 					
 		this._handleException(ex, "ZaZimbraAdmin.prototype.startup", null, true);
 	}
