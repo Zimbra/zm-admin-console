660f732bba5b326a939c51962a975fab00e6eb13
Greg Solovyev <greg@zimbra.com>
2009-09-01 15:37:50 -0500

    bug 40093 only check for single "@" in the email address instead of a full regexp check. Full regexp checks cannot be done for non-ASCII domain names
    http://bugzilla.zimbra.com/show_bug.cgi?id=40093
    
    Copied from Perforce
     Change: 183995

--- WebRoot/js/zimbraAdmin/accounts/controller/ZaAccountViewController.js
+++ WebRoot/js/zimbraAdmin/accounts/controller/ZaAccountViewController.js
@@ -407,7 +407,8 @@ function () {
 	var newName=null;
 	if(this._currentObject && tmpObj.name != this._currentObject.name) {
 		//var emailRegEx = /^([a-zA-Z0-9_\-])+((\.)?([a-zA-Z0-9_\-])+)*@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-		if(!AjxUtil.EMAIL_FULL_RE.test(tmpObj.name) ) {
+		//if(!AjxUtil.EMAIL_FULL_RE.test(tmpObj.name) ) {
+		if(tmpObj.name.lastIndexOf ("@")!=tmpObj.name.indexOf ("@")) {		
 			//show error msg
 			this._errorDialog.setMessage(ZaMsg.ERROR_ACCOUNT_NAME_INVALID, null, DwtMessageDialog.CRITICAL_STYLE, null);
 			this._errorDialog.popup();		
