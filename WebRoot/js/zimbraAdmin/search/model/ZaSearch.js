9bf7eaa687d7bb7b7f64f1297e281613f0b98bbd
Unknown Perforce User <jwagner>
2011-08-16 12:52:10 -0500

    Integrate change 325884 from HELIX to main
    -------------
    Bug: 63353 Need unique id for "The selected time is in the past" dialog (Send Later)
    Added id ShowPastDelayDialog
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=63353
    
    Copied from Perforce
     Change: 325885

--- WebRoot/js/zimbraAdmin/search/model/ZaSearch.js
+++ WebRoot/js/zimbraAdmin/search/model/ZaSearch.js
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Web Client
- * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011 Zimbra, Inc.
+ * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
  * 
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
@@ -99,6 +99,7 @@ ZaSearch.standardAttributes = [ZaAccount.A_displayname,
 							ZaAccount.A_accountStatus,
 							ZaAccount.A_zimbraLastLogonTimestamp,
 							ZaAccount.A_description,
+							ZaAccount.A_zimbraIsSystemAccount,
                             ZaAccount.A_zimbraIsDelegatedAdminAccount,
                             ZaAccount.A_zimbraIsAdminAccount,
                             ZaAccount.A_zimbraIsSystemResource,
@@ -358,7 +359,7 @@ ZaSearch.prototype.dynSelectSearchDomains = function (callArgs) {
 		params.callback = dataCallback;
 		params.sortBy = ZaDomain.A_domainName;
         	params.query = "";
-        	if(!ZaZimbraAdmin.isGlobalAdmin()) {
+        	if(!ZaZimbraAdmin.hasGlobalDomainListAccess()) {
             		var domainNameList = ZaApp.getInstance()._domainNameList;
             		if(domainNameList && domainNameList instanceof Array) {
                 		for(var i = 0; i < domainNameList.length; i++) {
@@ -424,7 +425,7 @@ ZaSearch.prototype.dynSelectSearchCoses = function (callArgs) {
 		params.callback = dataCallback;
 		params.sortBy = ZaCos.A_name;
                 params.query = "";
-                if(!ZaZimbraAdmin.isGlobalAdmin()) {
+                if(!ZaZimbraAdmin.hasGlobalCOSSListAccess()) {
                         var cosNameList = ZaApp.getInstance()._cosNameList;
                         if(cosNameList && (cosNameList instanceof Array) && cosNameList.length == 0) {
                             for(var i = 0; i < cosNameList.length; i++)
@@ -847,19 +848,19 @@ function (resp) {
 ZaSearch.loadPredefinedSearch =
 function () {
     if (ZaSearchField.canViewSavedSearch()) {
-        var currentSavedSearches = ZaSearch.getSavedSearches().Body.GetAdminSavedSearchesResponse.search;
-       
+        //var currentSavedSearches = ZaSearch.getSavedSearches().Body.GetAdminSavedSearchesResponse.search;
+        var currentSavedSearches = ZaApp.getInstance().getSavedSearchList();
         /*
          * If we get saved search from server and have write-permission, we will         * replace all the "zimbraIsDomainAdminAccount" with "zimbraIsDelegatedA         * dminAccount" to update the query string for version update      
          */ 
-        if ( currentSavedSearches && ( ZaSearchField.canSaveSearch() )){
+        if ( (!AjxUtil.isEmpty(currentSavedSearches)) && ( ZaSearchField.canSaveSearch() )){
             var modifiedSearches = [];
  
             for (var i = 0; i < currentSavedSearches.length; i++ ){
                var currentName = currentSavedSearches[i].name;
-               var currentContent = currentSavedSearches[i]._content;
+               var currentContent = currentSavedSearches[i].query;
     
-               if (currentContent.search(/zimbraIsDomainAdminAccount/) != -1){
+               if (currentContent && currentContent.search(/zimbraIsDomainAdminAccount/) != -1){
              
                   currentContent = currentContent.replace(/zimbraIsDomainAdminAccount/g , "zimbraIsDelegatedAdminAccount"); //'g' is used for global replace
                   modifiedSearches.push ({
@@ -871,10 +872,12 @@ function () {
             
             if ( modifiedSearches.length != 0 ){
                ZaSearch.modifySavedSearches (modifiedSearches);
+               ZaSearch.SAVED_SEARCHES = savedSearchArr;
+               ZaSearch._savedSearchToBeUpdated = false ;
             }   
         }
         
-        if ((! currentSavedSearches) && (ZaSearchField.canSaveSearch())){//load the predefined searches
+        if ((AjxUtil.isEmpty(currentSavedSearches)) && (ZaSearchField.canSaveSearch())){//load the predefined searches
             //if(window.console && window.console.log) console.log("Load the predefined saved searches ...") ;
             var savedSearchArr = [] ;
             //if (!ZaSettings.isDomainAdmin) { //admin only searches
@@ -888,6 +891,8 @@ function () {
             }
 
             ZaSearch.modifySavedSearches (savedSearchArr) ;
+            ZaSearch.SAVED_SEARCHES = savedSearchArr;
+            ZaSearch._savedSearchToBeUpdated = false ;
         }
     }
 }
