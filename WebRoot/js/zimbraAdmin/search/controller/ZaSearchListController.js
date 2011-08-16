a9b5cb83bff0b5a0d7e2c1a07fd67ea51659c23f
Unknown Perforce User <rajar>
2011-08-16 15:56:08 -0500

    bug:62400 Dont double encode filename
    http://bugzilla.zimbra.com/show_bug.cgi?id=62400
    
    Copied from Perforce
     Change: 325939

--- WebRoot/js/zimbraAdmin/search/controller/ZaSearchListController.js
+++ WebRoot/js/zimbraAdmin/search/controller/ZaSearchListController.js
@@ -190,16 +190,11 @@ function () {
 }
 
 ZaSearchListController.prototype._batchSearchforDomain =
-function (domainArr, searchQueryList, childQueries) {
+function (domainArr, searchQueryList) {
 	var paramsArr;
-    var searchTypes = ZaSearch.ALIASES;
-    var searchQuery = "(uid=*";
-    if(!childQueries) searchQuery += ")";
-    else if(childQueries && !(childQueries instanceof Array)) searchQuery += childQueries + "*)";
-    else if(childQueries instanceof Array && childQueries.length == 1) searchQuery += childQueries[0] + "*)";
-    else if(childQueries instanceof Array && childQueries.length > 1)  searchQuery += childQueries[1] + ")";
-
-    var controller = ZaApp.getInstance().getSearchListController();
+        var searchTypes = ZaSearch.ALIASES;
+        var searchQuery = "(uid=*)";
+        var controller = ZaApp.getInstance().getSearchListController();
 
 	if(searchQueryList && searchQueryList instanceof Array)
 		paramsArr = searchQueryList;
@@ -249,7 +244,7 @@ function(params,resp) {
                                 }
 				
                         }
-			ZaSearchListController.prototype._batchSearchforDomain(domainArr,params.searchQueryList, params.childQueries);
+			ZaSearchListController.prototype._batchSearchforDomain(domainArr,params.searchQueryList);
                 }
         } catch (ex) {
 		this._handleException(ex, "ZaSearchListController.searchAliasDomainCallback", null, false); 
@@ -260,17 +255,11 @@ function(params,resp) {
 ZaSearchListController.searchAliasDomain =
 function (value, searchCtl,searchQueryList) {
         var busyId = Dwt.getNextId();
-        var controller = searchCtl? searchCtl:this;
-
-        var query = "(" + ZaDomain.A_domainName;
-        if(value.length > 1) query += "=" + value[0] + "*)";
-        else query += "=*" + value[0] + "*)";
-
-        var callback = new AjxCallback(controller, ZaSearchListController.searchAliasDomainCallback,
-            {busyId:busyId, searchQueryList:searchQueryList, childQueries:value});
+	var controller = searchCtl? searchCtl:this;
+        var callback = new AjxCallback(controller, ZaSearchListController.searchAliasDomainCallback, {busyId:busyId, searchQueryList:searchQueryList});
         var searchParams = {
 
-                        query: query,
+                        query: "(" + ZaDomain.A_domainName + "=*" + value + "*)",
                         types: [ZaSearch.DOMAINS],
                         sortBy: ZaDomain.A_domainName,
                         attrs: [ZaDomain.A_domainName],
@@ -289,21 +278,16 @@ ZaSearchListController._getSearchKeyWord =
 function(query) {
 	var keyword = "";
 	var sw = "zimbraDomainName=*";
-        var domflag = "@";
 	var ew = "*";
 	if(!query) return keyword;
 	var start = query.indexOf(sw);
 	if(start < 0 || start > query.length-1)
-	    return [];
+		return keyword;
 	var end = query.indexOf(ew,start+sw.length);
 	if(end > query.length-1)
-	    return [];
-	keyword = query.substr(start+sw.length,end-start-sw.length);
-        // handle the case of "str@str"
-        start = keyword.indexOf(domflag);
-	if(start < 0 || start > query.length-1)
-	    return [keyword];
-	return [keyword.substr(start+1,keyword.length),keyword.substr(0,start)];
+		return keyword;
+	return query.substr(start+sw.length,end-start-sw.length);
+	
 }
 
 /*********** Search Field Callback */
@@ -916,17 +900,6 @@ function () {
 		if(this._popupOperations[ZaOperation.MOVE_ALIAS])	{
 			this._popupOperations[ZaOperation.MOVE_ALIAS].enabled = false;
 		}	
-	}
-        for(var i=0;i<cnt;i++) {
-        	var itemObj = this._contentView.getSelection()[i];
-        	if(itemObj && itemObj.type==ZaItem.ACCOUNT){
-                	if (itemObj.attrs[ZaAccount.A_zimbraIsSystemAccount] == "TRUE") {
-				if(this._toolbarOperations[ZaOperation.DELETE])
-                        		this._toolbarOperations[ZaOperation.DELETE].enabled=false;
-				if(this._popupOperations[ZaOperation.DELETE])
-                        		this._popupOperations[ZaOperation.DELETE].enabled = false;
-                	}
-		}
 	}
 }
 ZaController.changeActionsStateMethods["ZaSearchListController"].push(ZaSearchListController.changeActionsStateMethod);
