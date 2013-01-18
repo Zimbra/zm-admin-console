2c0f6232708fdc14fac6818a580cd63871299f53
Unknown Perforce User <jwagner>
2013-01-18 00:55:09 -0600

    Integrate change 437203 from zimbra to zimbra
    -------------
    Bug: 79735 Forwarding filter not shown in ZWC
    fix check for FORWARD that was always return false for hasValidAction
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=79735
    
    Copied from Perforce
     Change: 437204

--- WebRoot/js/zimbraAdmin/common/ZaCrtAppTreeHeader.js
+++ WebRoot/js/zimbraAdmin/common/ZaCrtAppTreeHeader.js
@@ -256,8 +256,6 @@ ZaCrtAppTreeHeader.prototype.createMenu = function
         mi.addSelectionListener(listener);
     }
 
-    this.menu.createSeparator();
-
     mi = new DwtMenuItem({
                     parent: this.menu,
                     style:		DwtMenuItem.NO_STYLE,
