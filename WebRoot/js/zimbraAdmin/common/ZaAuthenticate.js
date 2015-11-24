5154d415ddc643d51e38e21551a93d11290eace1
Conrad Damon <cdamon@zimbra.com>
2015-11-24 15:41:11 -0600

    Integrate change 560233 from zimbra to JUDASPRIEST
    -------------
    Bug: 102346 - line breaks lost when displaying invite having text context
    The issue is that the logic wasn't allowing a text/plain
    message with invite content to get converted to HTML.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=102346
    
    Copied from Perforce
     Change: 560234

--- WebRoot/js/zimbraAdmin/common/ZaAuthenticate.js
+++ WebRoot/js/zimbraAdmin/common/ZaAuthenticate.js
@@ -2,20 +2,20 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Web Client
  * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Common Public Attribution License Version 1.0 (the "License");
- * you may not use this file except in compliance with the License. 
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at: http://www.zimbra.com/license
- * The License is based on the Mozilla Public License Version 1.1 but Sections 14 and 15 
- * have been added to cover use of software over a computer network and provide for limited attribution 
- * for the Original Developer. In addition, Exhibit A has been modified to be consistent with Exhibit B. 
- * 
- * Software distributed under the License is distributed on an "AS IS" basis, 
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. 
- * See the License for the specific language governing rights and limitations under the License. 
- * The Original Code is Zimbra Open Source Web Client. 
- * The Initial Developer of the Original Code is Zimbra, Inc. 
- * All portions of the code are Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2012, 2013, 2014 Zimbra, Inc. All Rights Reserved. 
+ * The License is based on the Mozilla Public License Version 1.1 but Sections 14 and 15
+ * have been added to cover use of software over a computer network and provide for limited attribution
+ * for the Original Developer. In addition, Exhibit A has been modified to be consistent with Exhibit B.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * See the License for the specific language governing rights and limitations under the License.
+ * The Original Code is Zimbra Open Source Web Client.
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * All portions of the code are Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2012, 2013, 2014 Zimbra, Inc. All Rights Reserved.
  * ***** END LICENSE BLOCK *****
  */
 
@@ -25,15 +25,14 @@ ZaAuthenticate = function(appCtxt) {
     this.uname = "";
 }
 
-
 ZaAuthenticate.processResponseMethods = new Array();
 
-ZaAuthenticate.prototype.toString = 
+ZaAuthenticate.prototype.toString =
 function() {
     return "ZaAuthenticate";
 }
 
-ZaAuthenticate.prototype.changePassword = 
+ZaAuthenticate.prototype.changePassword =
 function (uname,oldPass,newPass,callback) {
     var soapDoc = AjxSoapDoc.create("ChangePasswordRequest", "urn:zimbraAccount");
     var el = soapDoc.set("account", uname);
@@ -43,13 +42,12 @@ function (uname,oldPass,newPass,callback) {
 
     var command = new ZmCsfeCommand();
     var params = new Object();
-    params.soapDoc = soapDoc;    
+    params.soapDoc = soapDoc;
     params.asyncMode = true;
     params.noAuthToken=true;
     params.callback = callback;
-    command.invoke(params);    
+    command.invoke(params);
 }
-
 ZaAuthenticate.prototype.execute =
 function (uname, pword, callback) {
     var soapDoc = AjxSoapDoc.create("AuthRequest", ZaZimbraAdmin.URN, null);
