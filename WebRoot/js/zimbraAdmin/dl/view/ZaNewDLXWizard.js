e679836035c72f284b5226b661710948167e76fe
Koichi Kato <koichi.kato@synacor.com>
2017-10-12 13:50:45 +0900

    ZCS-3104: chnaged the type of Reply-to address of DL
    An administrator can set any email address. It is unnecessary to use _DYNSELECT_ type.

--- WebRoot/js/zimbraAdmin/dl/view/ZaNewDLXWizard.js
+++ WebRoot/js/zimbraAdmin/dl/view/ZaNewDLXWizard.js
@@ -2310,32 +2310,11 @@ ZaNewDLXWizard.myXFormModifier = function(xFormObject, entry) {
                             enableDisableChangeEventSources: [ZaDistributionList.A_zimbraPrefReplyToEnabled]
                         },
                         {
-                            type: _DYNSELECT_,
+                            type: _TEXTFIELD_,
                             ref: ZaDistributionList.A_zimbraPrefReplyToAddress,
-                            dataFetcherClass: ZaSearch,
-                            dataFetcherMethod: ZaSearch.prototype.dynSelectSearch,
-                            dataFetcherTypes: [
-                                ZaSearch.ACCOUNTS,
-                                ZaSearch.RESOURCES,
-                                ZaSearch.DLS
-                            ],
-                            dataFetcherAttrs: [
-                                ZaItem.A_zimbraId,
-                                ZaItem.A_cn,
-                                ZaAccount.A_name,
-                                ZaAccount.A_displayname,
-                                ZaAccount.A_mail
-                            ],
                             label: ZaMsg.DLXV_ReplayToAddr,
-                            emptyText: ZaMsg.DLXV_ReplayToAddrEmptyText,
                             editable: true,
                             forceUpdate: true,
-                            choices: new XFormChoices(
-                                [],
-                                XFormChoices.OBJECT_LIST,
-                                "name",
-                                "name"
-                            ),
                             visibilityChecks: [
                                 [
                                     ZaItem.hasReadPermission,
@@ -2353,14 +2332,7 @@ ZaNewDLXWizard.myXFormModifier = function(xFormObject, entry) {
                                     ZaItem.hasWritePermission,
                                     ZaDistributionList.A_zimbraPrefReplyToAddress
                                 ]
-                            ],
-                            onChange: function (value, event, form) {
-                                if (value instanceof ZaItem ) {
-                                    this.setInstanceValue(value.name);
-                                } else {
-                                    this.setInstanceValue(value);
-                                }
-                            }
+                            ]
                         }
                     ]
                 }
