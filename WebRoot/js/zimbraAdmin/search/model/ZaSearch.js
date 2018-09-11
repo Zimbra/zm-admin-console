8a813d2c594599df273fdeb03dd45f0746923d47
Cataldo Mazzilli <cm@zextras.com>
2018-09-11 17:09:20 +0200

    ZXS-1758 fixed isGlobalAdmin check

--- WebRoot/js/zimbraAdmin/search/model/ZaSearch.js
+++ WebRoot/js/zimbraAdmin/search/model/ZaSearch.js
@@ -653,7 +653,7 @@ ZaSearch.getObjectCounts = function(types, callback) {
             type = "userAccount"; // exclude system account
         }
         getCountDoc.setAttribute("type", type);
-        if (ZaZimbraAdmin.isGlobalAdmin() === "FALSE" && type == ZaItem.DOMAIN && ZaZimbraAdmin.currentAdminAccount.attrs[ZaAccount.A_zimbraIsDelegatedAdminAccount] === "TRUE") {
+        if (ZaZimbraAdmin.isGlobalAdmin() === false && type == ZaItem.DOMAIN && ZaZimbraAdmin.currentAdminAccount.attrs[ZaAccount.A_zimbraIsDelegatedAdminAccount] === "TRUE") {
             getCountDoc.setAttribute("onlyRelated", "true");
         }
     }
