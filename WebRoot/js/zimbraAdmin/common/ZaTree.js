ef1c5268a02ddbca9e6c80be9a0d096b942d7763
sumitTelus <sumit.lnu@telusinternational.com>
2024-07-18 14:04:22 +0530

    ZCS-15551: Stopped user to navigate the page through keyboard arrow keys if license is not activated or any license feature attribute is disabled

--- WebRoot/js/zimbraAdmin/common/ZaTree.js
+++ WebRoot/js/zimbraAdmin/common/ZaTree.js
@@ -147,6 +147,15 @@ function(treeItem, skipNotify, kbNavEvent, noFocus) {
 	var da;
 	var j = 0;
 	var alreadySelected = false;
+	var isLicensedItem = false;
+	var isLicenseValid = false;
+	for (var i = 0; i < ZaTree.licenseCheckArray.length; i++) {
+		if (treeItem._text === ZaTree.licenseCheckArray[i].label) {
+			isLicenseValid = ZaTree.licenseCheckArray[i].licenseCheckFuncion.call();
+			isLicensedItem = true;
+			break;
+		}
+	}
 	for (var i = 0; i < sz; i++) {
 		if (a[i] == treeItem) {
 			alreadySelected = true;
@@ -171,7 +180,11 @@ function(treeItem, skipNotify, kbNavEvent, noFocus) {
 
 	//this._expandUp(treeItem);
 	if (treeItem._setSelected(true, noFocus) && !skipNotify) {
-    	this._notifyListeners(DwtEvent.SELECTION, [treeItem], DwtTree.ITEM_SELECTED, null, this._selEv, kbNavEvent);
+        if (!isLicensedItem || isLicenseValid) {
+            this._notifyListeners(DwtEvent.SELECTION, [treeItem], DwtTree.ITEM_SELECTED, null, this._selEv, kbNavEvent);
+        } else {
+            ZaApp.getInstance().getCurrentController().popupErrorDialog(ZaMsg.ERROR_FEATURE_NOT_LICENSED);
+        }
 	}
 };
 
